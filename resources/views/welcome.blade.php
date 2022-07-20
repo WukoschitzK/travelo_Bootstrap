94bd561df82d102cd8dcea7e8e32070501eac2f1
kerstin <kerstin.wukoschitz@gmx.net>
2022-07-20 15:06:46 +0200

    remove test in tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
         <div class="col-lg-7 wrapper d-flex flex-column justify-content-lg-center">
             <h1 class="fw-bold font-yellow mb-3">Bist du auch
                 Travelo?</h1>
-            <p class="mb-5 lh-sm fw-light subheadline">TEST Teile dein Erlebnis mit
+            <p class="mb-5 lh-sm fw-light subheadline">Teile dein Erlebnis mit
                 der ganzen Welt.</p>
             <p class="fw-light subheadline_text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore</p>
         </div>
